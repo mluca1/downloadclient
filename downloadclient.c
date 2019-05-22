88007e65701aaeebca15767d6cc52c84c93e50f8
Matt <mluca1@student.sierracollege.edu>
2019-05-22 06:38:29 +0000

    Files added and downloaded, Working

--- downloadclient.c
+++ downloadclient.c
@@ -1,52 +1,79 @@
 /*
     Matt Luca 
-    Date: 5/16/2019 @ 11:47am
+    Date: 5/21/2019 @ 11:26pm
     Subject: C Programming
     Lab 12: File Download Client.c
 */
 
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
-FILE * connect();
+#define ADDRESS "65.19.178.177"
+#define PORT 1234
+
+FILE * connect_to_server();
 void menu();
 char get_choice();
 void list_files(FILE *s);
 void download(FILE *s);
 void quit(FILE *s);
+int size(FILE *s, char *filename);
+void download_file(FILE *s, char *filename);
+void download_all(FILE *s);
+int list_all(FILE *s, char filenames[100][100]);
+void print_progress_bar(int left, int total);
+int file_exists(char *filename);
+void print_all_files_with_number(FILE *s);
 
 int main()
 {
     // Connect
-    FILE * s = connect();
+    FILE * s = connect_to_server();
     
-    // Menu
-    menu();
-    
-    // Get choice
-    char choice = get_choice();
-    
-    // Handle choice
-    switch(choice)
-    {
-        case 'l':
-        case 'L':
-            list_files(s);
-            break;
+    while(1) {
+        // Menu
+        menu();
         
-        case 'd':
-        case 'D':
-            download(s);
-            break;
+        // Get choice
+        char choice = get_choice();
+        
+        // Handle choice
+        switch(choice)
+        {
+            case 'l':
+            case 'L':
+                list_files(s);
+                break;
             
-        case 'q':
-        case 'Q':
-            quit(s);
-            exit(0);
-            break;
+            case 'd':
+            case 'D':
+                download(s);
+                break;
+                
+            case 'q':
+            case 'Q':
+                quit(s);
+                exit(0);
+                break;
             
-        default:
-            printf("Choice must be d, l, or q\n");
+            case 'a':
+            case 'A':
+                download_all(s);
+                break;
+
+            case 'n':
+            case 'N':
+                print_all_files_with_number(s);
+                break;
+
+            default:
+                printf("Choice must be d, l, or q\n");
+        }
     }
 }
 
@@ -54,9 +81,52 @@ int main()
  * Connect to server. Returns a FILE pointer that the
  * rest of the program will use to send/receive data.
  */
-FILE * connect()
+FILE * connect_to_server()
 {
-    return NULL;
+    // Create buffer and necessary variables
+    char buffer[200];
+    struct sockaddr_in address;
+    int sock = 0;
+    FILE * fp = NULL;
+    struct sockaddr_in serv_addr;
+
+    // Creating socket file descriptor 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("Socket creation error!\n"); 
+        return NULL;
+    }
+
+    // Set socket to zero
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    // Configure socket
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 address from text to binary form 
+    if(inet_pton(AF_INET, ADDRESS, &serv_addr.sin_addr)<=0)  
+    { 
+        printf("Address not supported!\n"); 
+        return NULL; 
+    }
+
+    // Connect socket
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("Connection Failed!\n"); 
+        return NULL; 
+    }
+
+    // Convert socket stream to file stream
+    fp = fdopen(sock, "r+");
+
+    // Read incoming message
+    fgets(buffer, sizeof(buffer), fp);
+    printf("%s", buffer);
+
+    // Return file descriptor
+    return fp;
 }
 
 /*
@@ -64,8 +134,11 @@ FILE * connect()
  */
 void menu()
 {
+    printf("\n\n");
     printf("List files\n");
     printf("Download a file\n");
+    printf("All files\n");
+    printf("Numbered download\n");
     printf("Quit\n");
     printf("\n");
 }
@@ -88,7 +161,21 @@ char get_choice()
  */
 void list_files(FILE *s)
 {
-    
+    // Create a buffer to read respond
+    char buffer[200];
+
+    // Send command LIST to server
+    fprintf(s, "LIST\n");
+
+    // Flush fprintf
+    fflush(s);
+
+    // Read back the received data and print until we find ".\n"
+    while ( fgets(buffer, sizeof(buffer), s) ) {
+        if (strcmp(buffer, ".\n") == 0)
+            break;
+        printf("%s", buffer);
+    }
 }
 
 /*
@@ -97,7 +184,6 @@ void list_files(FILE *s)
  * Download it from the server and save it to a file with the
  * same name.
  */
- 
 void download(FILE *s)
 {
     
@@ -110,3 +196,4 @@ void quit(FILE *s)
 {
     fclose(s);
 }
+
